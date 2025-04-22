2784cd0351bd2a820c7693d2c8610471f6868229
Mustafa Fatih Gündüz <119396849+mustafatihgunduz@users.noreply.github.com>
2025-04-22 10:53:55 +0300

    Initial commit

---
+++ newsletter/components/views/FooterView.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FooterView = () => {
+  return (
+    <footer>
+      <div className="footer-bottom py-2">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <p className="mb-0 pb-3 d-flex justify-content-center">
+                Copyright ©2025 All rights reserved | This website is made with
+                by Fatih and Fahri
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterView;
